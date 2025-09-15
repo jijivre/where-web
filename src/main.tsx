a02a39c5762cc9207fefd72b2e5da5fdc5962d41
Railas9 <railasben@gmail.com>
2025-09-15 10:44:51 +0200

    lobby + call

--- src/main.tsx
+++ src/main.tsx
@@ -12,8 +12,9 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/lobby" element={<Lobby />} />
+        
         <Route path="/call" element={<Call />} />
-      </Routes>
+      </Routes>     
     </BrowserRouter>
   </StrictMode>,
 );
