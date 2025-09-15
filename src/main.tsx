94b4557250d88f989ef34182ccf0158f9e8dff67
Railas9 <railasben@gmail.com>
2025-09-15 10:47:20 +0200

    fix merging 2

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,6 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/lobby" element={<Lobby />} />
-        
         <Route path="/call" element={<Call />} />
       </Routes>     
     </BrowserRouter>
