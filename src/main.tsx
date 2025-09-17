c52c191c4e4922be30fbfa1c98f60dba8109ceb7
Adam <adamboissy2004@gmail.com>
2025-09-17 20:25:51 +0200

    oups bad error

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router';
 import App from './App.tsx';
 import Call from './Call.tsx';
 import Lobby from './Lobby.tsx';
