6c888af54d9d6bab08dc4ae6926ca257077602bd
Adam <adamboissy2004@gmail.com>
2025-09-17 20:20:51 +0200

    log retiré

--- src/main.tsx
+++ src/main.tsx
@@ -6,14 +6,6 @@ import Call from './Call.tsx';
 import Lobby from './Lobby.tsx';
 import './index.css';
 
-const NotFound = () => (
-  <div style={{ padding: 20, textAlign: 'center' }}>
-    <h1>404 - Page non trouvée</h1>
-    <p>La page que vous cherchez n'existe pas.</p>
-    <a href="/">Retour à l'accueil</a>
-  </div>
-);
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HashRouter>
@@ -21,7 +13,6 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<App />} />
         <Route path="/lobby" element={<Lobby />} />
         <Route path="/call" element={<Call />} />
-        <Route path="*" element={<NotFound />} />
       </Routes>
     </HashRouter>
   </StrictMode>,
